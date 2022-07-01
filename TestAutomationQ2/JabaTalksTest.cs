91285d179e7df5dd877dac41e21d315779de330b
shubham vishwakarma <shubhbj11@gmail.com>
2022-07-01 11:02:04 +0530

    Assignment Question2

---
+++ TestAutomationQ2/JabaTalksTest.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestAutomationQ2
+{
+    [TestClass]
+    public class JabaTalksTest
+    {
+
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void InIt()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://jt-dev.azurewebsites.net/#/SignUp");
+            driver.Manage().Window.Maximize();
+        }
+
+        [DataRow("English,Dutch")]
+        [TestMethod]
+        public void ValidateLanguagedropdown(string languages)
+        {
+            try
+            {
+                driver.FindElement(By.XPath("//div[@placeholder='Choose Language']/span")).Click();
+                List<IWebElement> elements = driver.FindElements(By.XPath("//li[@class='ui-select-choices-group']/div[contains(@id,'ui-select-choices-row')]")).ToList<IWebElement>();
+                foreach (IWebElement item in elements)
+                {
+                    if (languages.Contains(item.Text))
+                        Assert.IsTrue(true);
+                    else
+                        Assert.Fail();
+                }
+            }
+            catch (Exception e)
+            {
+                _ = e.StackTrace;
+            }
+
+        }
+
+        [TestMethod]
+        public void ValidateSubmitButtonShouldBeDisableByDefault()
+        {
+            try
+            {
+                string value = driver.FindElement(By.XPath("//button[@type='submit' and text()='Get Started']")).GetAttribute("disabled");
+                if (value.Equals("disabled"))
+                    Assert.IsTrue(true);
+                else
+                    Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+
+        [DataRow("Shubham Vishwakarma", "Shubham", "shubham@gmail.com")]
+        [TestMethod]
+        public void ValidateSubmitButtonShouldBeEnabledOnceAllDetailsEntered(string name, string organization, string email)
+        {
+            try
+            {
+                EnterLoginDetails(name, organization, email);
+                driver.FindElement(By.XPath("//input[@type='checkbox']/following::span[contains(text(),'I agree to the')]")).Click();
+                string value = driver.FindElement(By.XPath("//button[@type='submit' and text()='Get Started']")).GetAttribute("disabled");
+                if (value == null)
+                    Assert.IsTrue(true);
+                else
+                    Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+        [DataRow("Shubham Vishwakarma", "Shubham", "shubham@gmail.com")]
+        [TestMethod]
+        public void ValidateSignUp(string name, string organization, string email)
+        {
+            try
+            {
+                EnterLoginDetails(name, organization, email);
+                driver.FindElement(By.XPath("//input[@type='checkbox']/following::span[contains(text(),'I agree to the')]")).Click();
+                driver.FindElement(By.XPath("//button[@type='submit' and text()='Get Started']")).Click();
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//span[contains(text(),' A welcome email has been sent. Please check your email. ')]")));
+                if (driver.FindElement(By.XPath("//input[@type='checkbox']/following::span[contains(text(),'I agree to the')]")).Displayed)
+                    Assert.IsTrue(true);
+                else
+                    Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+        [TestMethod]
+        public void ValidateTermsAndConditionLink()
+        {
+            try
+            {
+
+                driver.FindElement(By.PartialLinkText("Terms and Conditions")).Click();
+                driver.SwitchTo().Window(driver.WindowHandles[1]);
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+                driver.FindElement(By.XPath("//header/h1[contains(text(),'Jabatalks.co')]"));
+                //header/h1[contains(text(),'Jabatalks.co')]
+                if (driver.Title.Equals("Jabatalks.co"))
+                    Assert.IsTrue(true);
+                else
+                    Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+        public void EnterLoginDetails(string name, string org, string email)
+        {
+            try
+            {
+                driver.FindElement(By.Id("name")).SendKeys(name);
+                driver.FindElement(By.Id("orgName")).SendKeys(org);
+                driver.FindElement(By.Id("singUpEmail")).SendKeys(email);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup() => driver.Quit();
+
+    }
+}
---
+++ TestAutomationQ2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAutomationQ2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAutomationQ2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2176a1e1-d7b4-437d-987f-3dee8f64a177")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
